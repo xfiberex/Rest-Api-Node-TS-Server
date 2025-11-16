517ba89b5c2ef6a418b249245ef55365b90bac60
xfiberex <rickystrong79@gmail.com>
2025-11-16 19:25:41 -0400

    Añadir caracteristicas de seguridad y dependencias
    - CORS y registro de peticiones con Morgan.
    
    - Actualizar dependencias en package.json y package-lock.json

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL!, {
-    dialectOptions: {
-        ssl: {
-            require: false,
-        },
-    },
+// Base de datos de producción PostgreSQL
+// const db = new Sequelize(process.env.DATABASE_URL!, {
+//     dialectOptions: {
+//         ssl: {
+//             require: false,
+//         },
+//     },
+//     models: [__dirname + "./../models/**/*.ts"],
+//     logging: false,
+// });
+
+// Base de datos de desarrollo PostgreSQL
+const db = new Sequelize(process.env.DATABASE_URL_TEST!, {
     models: [__dirname + "./../models/**/*.ts"],
     logging: false,
 });
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import swaggerUI from "swagger-ui-express";
 import swaggerSpec, { swaggerUIOptions } from "./config/swagger";
 import db from "./config/db";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
+import { error } from "console";
 
 // Conectar a base de datos
 export async function connectDB() {
@@ -26,13 +29,32 @@ if (process.env.NODE_ENV !== "test") {
 
 const server = express();
 
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if (origin === process.env.FRONTEND_URL) {
+            callback(null, true);
+        } else {
+            callback(new error("Errors de CORS"));
+        }
+    },
+};
+server.use(cors(corsOptions));
+
 // Leer datos de formularios
 server.use(express.json());
+
+// Mas detalles sobre las peticiones en consola
+server.use(morgan("dev"));
 
 // Englobar metodos REST API desde router.ts
 server.use("/api/products", router);
 
 // Documentación de API's con Swagger
-server.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec, swaggerUIOptions));
+server.use(
+    "/docs",
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerSpec, swaggerUIOptions)
+);
 
 export default server;
